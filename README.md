7e168ee284b533e031cd92c4fa0e31c3e95b7633
Grayson-code <northernstarmusic10@gmail.com>
2022-03-05 10:41:06 +0530

    publish

---
+++ README.md
@@ -0,0 +1,40 @@
+# cmc-checker
+
+-[License](./LICENSE)
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+
+## About <a name = "about"></a>
+
+cmc-checker is a package that checks the status of Cracked Minecraft Club a minecraft server, the
+[Discord](https://discord.gg/FpMDe8Ge5g)
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions Will help you through production
+
+### Instructions
+
+Install the package
+
+```console
+npm i cmc-checker
+```
+
+### Using the Package
+
+Function
+```ts
+await isOnline()
+```
+
+Returns A JSON data about the data of the server
+
+
+## Usage <a name = "usage"></a>
+
+You can use this in bots that check the availability of the server and also the data of it.
---
+++ src/main.ts
@@ -0,0 +1,56 @@
+// Copyright (c) 2022 Northern Star
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import checker from 'minecraft-pinger'
+export namespace status {
+  export async function isOnline () {
+    const eu = await checker.pingPromise('mc.crackedminecraft.club', 25565)
+    const ap = await checker.pingPromise('ind.crackedminecraft.club', 25565)
+    let euOnline: boolean
+    let apOnline: boolean
+    if (eu.ping) {
+      euOnline = true
+    }
+    if (ap.ping) {
+      apOnline = true
+    }
+    return {
+      /**
+       * CMCs Eu Data
+       */
+      eu: {
+        /**
+         * If eu node is online
+         */
+        online: euOnline,
+        /**
+         * The latency your getting
+         */
+        ping: eu.ping,
+        /**
+         * The Timestamp of function execution in unix timestamp
+         */
+        timestamp: Math.round(new Date().getTime() / 1000)
+      },
+      /**
+       * AP Nodes Data
+       */
+      ap: {
+        /**
+         * if  ap node is online
+         */
+        online: apOnline,
+        /**
+         * The latency your gettubg
+         */
+        ping: ap.ping,
+        /**
+         * The Timestamp of function execution in unix timestamp
+         */
+        timestamp: Math.round(new Date().getTime() / 1000)
+      }
+    }
+  }
+}
